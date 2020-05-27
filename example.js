9a79ef4617b780746aa4893ce3ebecd9af2a4cf7
Ines Pignatelli <ipignatelli@gmail.com>
2020-05-27 16:46:28 +0100

    small changes

--- example.js
+++ example.js
@@ -1,14 +1,36 @@
 var STEP = require("./readStep");
-
+var CartesianPointKeeper = require("./CartesianPointKeeper");
 var reader = new STEP.StepReader();
 
+var opitzCode = {
+    firstDigit: '-1',
+    secondDigit: '-1',
+    thirdDigit: '-1',
+    fourthDigit: '-1',
+    fifthDigit: '-1',
+    sixthDigit: '-1',
+    seventhDigit:'-1',
+    eighthDigit: '-1',
+    ninethDigit: '-1'
+}
+
+let csLineId = [];
 reader.read("parts/anchor.step",function(err) {
     if (err) {
         console.log("failure :" + err);
         return;
     }
     var product_definitions = reader.getObjects("PRODUCT_DEFINITION");
-    reader.dumpStatistics();
+
+    let cs = reader.getObjects("CLOSED_SHELL");
+    cs.forEach(element => {
+        csLineId.push(element['_id']);
+    });
+
+    let maxMeasures = CartesianPointKeeper.getMaxShapeMeasures();
+    //reader.dumpStatistics();
 });
+
+
 
 
